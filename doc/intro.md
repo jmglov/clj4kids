b7cae39ff930e84c30ebe327f529c81c5388e85a
Josh Glover <jmglov@gmail.com>
2015-08-31 17:38:12 +0200

    Remove unnecessary cruft

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to clj4kids
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/clj4kids/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj4kids.core-test
-  (:require [clojure.test :refer :all]
-            [clj4kids.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
