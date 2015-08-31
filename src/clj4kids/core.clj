21fc498b578649cb2bb0bfa7c84906a6ba79f9d1
Josh Glover <jmglov@gmail.com>
2015-08-31 15:01:36 +0200

    Add x- and y-axis labels

--- src/clj4kids/core.clj
+++ src/clj4kids/core.clj
@@ -1,14 +1,13 @@
 (ns clj4kids.core
   (:require [clj4kids.graphics :as gfx]))
 
-(def width 40)
-(def height 40)
+(def side-length 40)
 (def pixel-size 16)
 
 (def clear-colour :black)
 (def default-colour :white)
 
-(def ^:private clear-screen (vec (repeat height (vec (repeat width clear-colour)))))
+(def ^:private clear-screen (vec (repeat side-length (vec (repeat side-length clear-colour)))))
 
 (defonce ^:private raster (atom clear-screen))
 
@@ -16,8 +15,15 @@
   {:black [0 0 0]
    :white [255 255 255]})
 
+(defn make-panel []
+  (gfx/make-panel colours raster side-length pixel-size))
+
 (defonce ^:private canvas
-  (delay (gfx/make-panel colours raster width height pixel-size pixel-size)))
+  (delay (make-panel)))
+
+(defn clear []
+  (force canvas)
+  (reset! raster clear-screen))
 
 (defn dot [x y]
   (force canvas)
--- src/clj4kids/graphics.clj
+++ src/clj4kids/graphics.clj
@@ -1,33 +1,47 @@
 (ns clj4kids.graphics
-  (:import (java.awt Dimension Color)
-           (javax.swing JPanel JFrame)))
+  (:import (java.awt BorderLayout Color Dimension GridLayout)
+           (javax.swing JFrame JLabel JPanel)))
 
 (defn- rgb->color
   [[r g b]]
   (Color. r g b))
 
+(defn make-container [side-length]
+  (let [increment (/ side-length 10)
+        container (JPanel. (BorderLayout.))
+        x-axis (JPanel. (GridLayout. 1 10))
+        y-axis (JPanel. (GridLayout. 10 1))]
+    (doseq [offset (range 10)]
+      (.add x-axis (JLabel. (str (* offset increment)) JLabel/CENTER))
+      (.add y-axis (doto (JLabel. (str (* offset increment)) JLabel/CENTER)
+                     (.setVerticalAlignment JLabel/CENTER))))
+    (doto container
+      (.add x-axis BorderLayout/PAGE_START)
+      (.add y-axis BorderLayout/LINE_START))))
+
 (defn make-panel
-  [color-map raster width height pixel-width pixel-height]
+  [color-map raster side-length pixel-size]
   (let [frame (JFrame. "clj4kids")
         rgb->color (memoize rgb->color)
         paint-point (fn [x y c g]
                       (.setColor g (rgb->color (c color-map)))
                       (.fillRect g
-                                 (* x pixel-width) (* y pixel-height)
-                                 pixel-width pixel-height))
+                                 (* x pixel-size) (* y pixel-size)
+                                 pixel-size pixel-size))
         panel (proxy [JPanel] []
                 (paintComponent [g]
                   (proxy-super paintComponent g)
                   (let [r @raster]
-                    (doseq [x (range width)
-                            y (range height)]
+                    (doseq [x (range side-length)
+                            y (range side-length)]
                       (paint-point x y (get-in r [x y]) g))))
                 (getPreferredSize []
                   (Dimension.
-                   (* width pixel-width)
-                   (* height pixel-height))))]
+                   (* side-length pixel-size)
+                   (* side-length pixel-size))))]
     (doto frame
-      (.add panel)
+      (.add (doto (make-container side-length)
+              (.add panel BorderLayout/CENTER)))
       (.pack)
       (.setVisible true))
     (add-watch raster :monitor
