f0e43f7996f98264584de5431d19bdab6e4f8f28
Josh Glover <jmglov@gmail.com>
2015-09-20 18:52:30 +0200

    Add move, save, and load

--- src/clj4kids/core.clj
+++ src/clj4kids/core.clj
@@ -14,7 +14,7 @@
 
 (def colors
   {:black       [0 0 0]
-   :red         [157 9 102]
+   :red         [255 0 0]
    :dark-blue   [42 42 229]
    :purple      [199 52 255]
    :dark-green  [0 118 26]
@@ -39,26 +39,71 @@
 (defn color [c]
   (reset! current-color c))
 
-(defn clear []
-  (force canvas)
-  (reset! raster clear-screen)
-  nil)
-
 (defn point [x y]
   (force canvas)
   (swap! raster assoc-in [x y] @current-color)
-  nil)
+  :ok)
+
+(defn clear
+  ([]
+   (force canvas)
+   (reset! raster clear-screen)
+   :ok)
+
+  ([x y]
+   (force canvas)
+   (let [c @current-color]
+     (color clear-color)
+     (point x y)
+     (color c))
+   :ok))
 
 (defn h-line [y x1 x2]
   (doseq [x (range x1 (inc x2))]
     (point x y))
-  nil)
+  :ok)
 
 (defn v-line [x y1 y2]
   (doseq [y (range y1 (inc y2))]
     (point x y))
-  nil)
+  :ok)
 
 (defn box [x1 y1 x2 y2]
   (doseq [y (range y1 y2)]
-    (h-line y x1 x2)))
+    (h-line y x1 x2))
+  :ok)
+
+(defn move
+  [x y]
+  "Moves the entire drawing x pixels right and y pixels down. To move left or up,
+  use a negative number for x and/or y."
+  (let [abs #(if (pos? %) % (* % -1))
+        transform (fn [n c] (if (neg? n)
+                             (drop (abs n) c)
+                             (take (- side-length (abs n)) c)))
+        fill-rows (take (abs x) clear-screen)
+        fill-pixels (repeat (abs y) clear-color)]
+    (->> (concat (when (pos? x) fill-rows)
+                 (transform x @raster)
+                 (when-not (pos? x) fill-rows))
+         (map (fn [col] (vec (concat (when (pos? y) fill-pixels)
+                                    (transform  y col)
+                                    (when-not (pos? y) fill-pixels)))))
+         vec
+         (reset! raster))))
+
+(defn- drawing-filename [name]
+  (format "drawing-%s.clj" name))
+
+(defn save [name]
+  (spit (drawing-filename name)
+        (pr-str @raster))
+  :ok)
+
+(defn load [name]
+  (force canvas)
+  (->> (drawing-filename name)
+       slurp
+       read-string
+       (reset! raster))
+  :ok)
