6e683b5954c18f3c908d8200acece0e24e86af72
Josh Glover <jmglov@gmail.com>
2015-09-20 19:28:24 +0200

    Add documentation to functions

--- src/clj4kids/core.clj
+++ src/clj4kids/core.clj
@@ -1,5 +1,6 @@
 (ns clj4kids.core
-  (:require [clj4kids.graphics :as gfx]))
+  (:require [clj4kids.graphics :as gfx]
+            [clojure.string :as string]))
 
 (def ^:private side-length 40)
 (def ^:private pixel-size 16)
@@ -36,15 +37,23 @@
 (defonce ^:private canvas
   (delay (make-panel)))
 
-(defn color [c]
-  (reset! current-color c))
+(defn color
+  "Use the color that you name. To see all the colors you can use, just
+  enter: (color)."
+  ([] (println (string/join "\n" (keys colors))))
+  ([name]
+   (reset! current-color name)))
 
-(defn point [x y]
+(defn point
+  "Draw a point at coordinates (x, y). Ask your math teacher or parents to explain."
+  [x y]
   (force canvas)
   (swap! raster assoc-in [x y] @current-color)
   :ok)
 
 (defn clear
+  "Clears the whole screen. If you want to just clear one point (maybe you made a
+  mistake?), then call clear with x and y."
   ([]
    (force canvas)
    (reset! raster clear-screen)
@@ -58,25 +67,32 @@
      (color c))
    :ok))
 
-(defn h-line [y x1 x2]
+(defn h-line
+  "Draws a horizontal line from (x1, y) to (x2, y)."
+  [y x1 x2]
   (doseq [x (range x1 (inc x2))]
     (point x y))
   :ok)
 
-(defn v-line [x y1 y2]
+(defn v-line
+  "Draws a vertical line from (x, y1) to (x, y2)."
+  [x y1 y2]
   (doseq [y (range y1 (inc y2))]
     (point x y))
   :ok)
 
-(defn box [x1 y1 x2 y2]
+(defn box
+  "Draws a box with an upper left-hand corner at (x1, y1) and a lower right-hand
+  corner at (x2, y2)."
+  [x1 y1 x2 y2]
   (doseq [y (range y1 y2)]
     (h-line y x1 x2))
   :ok)
 
 (defn move
-  [x y]
   "Moves the entire drawing x pixels right and y pixels down. To move left or up,
   use a negative number for x and/or y."
+  [x y]
   (let [abs #(if (pos? %) % (* % -1))
         transform (fn [n c] (if (neg? n)
                              (drop (abs n) c)
@@ -90,20 +106,45 @@
                                     (transform  y col)
                                     (when-not (pos? y) fill-pixels)))))
          vec
-         (reset! raster))))
+         (reset! raster)))
+  :ok)
 
 (defn- drawing-filename [name]
   (format "drawing-%s.clj" name))
 
-(defn save [name]
+(defn save
+  "Saves the drawing with a name."
+  [name]
   (spit (drawing-filename name)
         (pr-str @raster))
   :ok)
 
-(defn load [name]
+(defn load
+  "Loads the drawing with the name."
+  [name]
   (force canvas)
   (->> (drawing-filename name)
        slurp
        read-string
        (reset! raster))
   :ok)
+
+(defn help
+  "Display all commands and explain how to get more help."
+  []
+  (->> ["clj4kids lets you make simple drawings.\n"
+        "The commands you can use are:\n"
+        "  clear"
+        "  color"
+        "  point"
+        "  box"
+        "  h-line"
+        "  v-line"
+        "  move"
+        "  save"
+        "  load"
+        "\n"
+        "To learn more about each command, use the (doc) command, like this:\n"
+        "  (doc color)"]
+       (string/join "\n")
+       println))
